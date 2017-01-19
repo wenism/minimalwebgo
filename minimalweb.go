91c747925f7b028ec04c359b53c40a0fd895f9af
wenism <whyeow@gmail.com>
2017-01-20 08:55:37 +1100

    Test

--- minimalweb.go
+++ minimalweb.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-    "html/template"
-    "log"
-    "log/syslog"
     "os"
     "net/http"
     "strings"
@@ -11,64 +8,17 @@ import (
 
 // This is injected at build time
 var AppVersion = "undefined"
-var BuiltOn = "undefined"
-var BuiltUsing = "undefined"
-
-// This is injected at runtime
-var environment = os.Getenv("ENVIRONMENT") 
-var containerEngineVersion = os.Getenv("CONTAINER_ENGINE_VERSION")
-var operatingSystem = os.Getenv("OS")
-var cloudProvider = os.Getenv("CLOUD_PROVIDER")
-
-var templates = template.Must(template.ParseFiles("hello.template.html"))
-
-type Model struct {
-    Environment string
-    AppVersion string
-    BuiltOn string
-    BuiltUsing string
-    ContainerEngineVersion string
-    OperatingSystem string
-    CloudProvider string
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, m *Model) {
-    err := templates.ExecuteTemplate(w, tmpl+".template.html", m)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
-
-func getModel() *Model {
-    return &Model{Environment: environment,
-		AppVersion: AppVersion,
-	    BuiltOn: BuiltOn,
-	    BuiltUsing: BuiltUsing,
-	    ContainerEngineVersion: containerEngineVersion,
-	    OperatingSystem: operatingSystem,
-      CloudProvider: cloudProvider}
-}
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "hello", getModel())
-	
-    log.Print("responding to / request from " + strings.Split(r.RemoteAddr,":")[0])
+	fmt.Fprintf(w, "<h1>Minimal Web in Go</h1><p>Version: %s </p>", AppVersion)
 }
 
-func setupLog() {
-    // Configure logger to write to the syslog. You could do this in init(), too.
-    logwriter, e := syslog.New(syslog.LOG_NOTICE, "minimalwebgo")
-    if e == nil {
-        log.SetOutput(logwriter)
-    }
+func handleVersion(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s", AppVersion)
 }
 
 func main() {
-  setupLog()
-    
-  log.Printf("Starting application version %s on %s environment", AppVersion, environment)
-    
 	http.HandleFunc("/", handleIndex)
-
-	http.ListenAndServe(":9999", nil)
+  http.HandleFunc("/version", handleVersion)
+	http.ListenAndServe(":80", nil)
 }
