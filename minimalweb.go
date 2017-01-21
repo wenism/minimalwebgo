7764965118847c1b6beb972eb888c2641152766a
wenism <whyeow@gmail.com>
2017-01-21 14:58:41 +1100

    Test

--- minimalweb.go
+++ minimalweb.go
@@ -18,7 +18,7 @@ func handleVersion(w http.ResponseWriter, r *http.Request) {
 
 func handleLiveness(w http.ResponseWriter, r *http.Request) {
   // running?
-	fmt.Fprintf(w, "OK\n")
+	fmt.Fprintf(w, "<p>ok</p>\n")
 }
 
 func handleReadiness(w http.ResponseWriter, r *http.Request) {
